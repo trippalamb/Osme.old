fac7cc9fe18aca6789a9fda464a3fb4e74d9afbd
Tripp Lamb <trippalamb@gmail.com>
2017-10-10 17:48:56 -0500

    added if statement and nested control struture support

--- Compiler/Osme.js
+++ Compiler/Osme.js
@@ -1,6 +1,6 @@
-var nodejs = process.argv[0];
-//var file = process.argv[1];
-var file = "C:/Users/tripp.lamb/Projects/Osme/ProjectEulerExamples/Osme/Problem1.osme";
+var nodejs = process.argv[1];
+var file = process.argv[2];
+//var file = "C:/Users/tripp.lamb/Projects/Osme/ProjectEulerExamples/Osme/Problem1.osme";
 var jsfile = file.slice(0, file.lastIndexOf('.')) + ".js";
 
 compile(file, jsfile)//, "osme", "js")
@@ -9,79 +9,19 @@ compile(file, jsfile)//, "osme", "js")
 function compile(file, outfile){
 
     var Expander = require('./expander.js');
-    
     var fs = require('fs');
 
     
 
-    var program  = [];
-    var j = 0;
-
     content = fs.readFileSync(file, 'utf8');
-    lines = content.split("\r\n");
-    var lessContent = "";
+    code = decode(content);
     
-    for(var i = 0; i< lines.length; i++){
 
-        lineInfo = getLineInfo(lines[i]);
-        if(lineInfo.multiline){
-            lessContent = lines.slice(i).join("\r\n");
-            program[j] = multilineMatch(lineInfo.opening, lessContent);
-            i += program[j].lines;
-            
-            j++;
-
-        }
-        else{
-            program[j] = lines[i];
-            j++;
-        }
-
-
-    }
-
-    console.log(program);
+    console.log(code);
+    console.log(code[4]);
 
     //fs.writeFileSync(outfile, content);
     //console.log("Conversion Complete");
-
-}
-
-function getLineInfo(line){
-
-    var reg = /(do|if)/;
-    var m = line.match(reg);
-    var lineInfo = {};
-
-    if(m !== null){
-        lineInfo.multiline = true;
-        lineInfo.opening = m[1];
-    }
-    else{
-        lineInfo.multiline = false;
-    }
-
-    return lineInfo;
-}
-
-function multilineMatch(opening, content){
-
-    var reg;
-    var code = {};
-    if(opening === "do"){
-        reg = /do\s+(\w)\s*(=\s*\d+\s*\.\.\.\s*\d+)([\s\S]*)end\s+do(.*)/mi;
-        m = content.match(reg);
-        code = {
-            structure: "do",
-            whole:         m[0],
-            iterative:     m[1],
-            iterativeVals: m[2],
-            inner:         m[3],
-            extra:         m[4],
-            lines:         5
-        }
-    }
-    return code;
 
 }
 
@@ -94,6 +34,134 @@ function run(nodejs, jsfile){
         if(stdout !== null){console.log(stdout);}
         if(stderr !== null){console.log(stderr);}
     });
+
+}
+
+//TODO: make a translator file
+function decode(content){
+    lines = content.split("\r\n");
+    var lessContent = "";
+    var code = [];
+    var j = 0;
+    
+    for(var i = 0; i< lines.length; i++){
+
+        lineInfo = getLineInfo(lines[i]);
+        if(lineInfo.isContainer){
+            console.log(lineInfo.opening);
+            lessContent = lines.slice(i).join("\r\n");
+            code[j] = containerMatch(lineInfo.opening, lessContent);
+            i += code[j].lines;
+            
+            j++;
+
+        }
+        else{
+            code[j] = lines[i];
+            j++;
+        }
+
+
+    }
+
+    return code;
+}
+
+
+//todo: make part of the Osme plugin
+function getLineInfo(line){
+
+    var reg = /(do|if)/;
+    var m = line.match(reg);
+    var lineInfo = {};
+
+    if(m !== null){
+        lineInfo.isContainer = true;
+        lineInfo.opening = m[1];
+    }
+    else{
+        lineInfo.isContainer = false;
+    }
+
+    return lineInfo;
+}
+
+//todo: make part of the Osme plugin
+function containerMatch(opening, content){
+
+    var reg;
+    var code = {};
+    if(opening === "do"){
+        reg = /do\s+(\w)\s*(=\s*\d+\s*\.\.\.\s*\d+)([\s\S]*)end\s+do(.*)/mi;
+        m = content.match(reg);
+        code = new Code_DoLoop(m);
+    }
+    else if(opening === "if"){
+        reg = /if\s*\(([\s\S]*?)\)([\s\S]*?)(?:else\s*if\s*\(([\s\S]*?)\)([\s\S]*?))*(?:else([\s\S]*?))?end\s*if/mi;
+        m = content.match(reg);
+        code = new Code_IfStatement(m);
+    }
+    else{
+        console.log("error in multiline match");
+    }
+
+    return code;
+
+}
+
+
+
+
+//todo: make part of the Osme plugin
+function Code_DoLoop(m, content){
+
+
+
+    this.structure     = "do";
+    this.whole         = m[0];
+    this.iterative     = m[1];
+    this.iterativeVals = m[2];
+    this.inner         = m[3];
+    this.extra         = m[4];
+    this.lines         = m[0].match(/\r/g).length;; //this is really - 1 (for 0 match) and + 1(for end of last matched line)
+
+    this.code          = decode(this.inner);
+}
+//todo: make part of the Osme plugin
+function Code_IfStatement(m, content){
+
+    for(var i = 0; i < m.length; i++){
+        if(typeof(m[i]) === "undefined"){
+            m.splice(i, 1);
+            i--;
+        }
+    }
+    this.structure = "if";
+    this.lines = m[0].match(/\r/g).length;
+    this.whole = m.shift();
+
+    this.conditionals  = [];
+    this.inners = [];
+
+    if(m.length % 2 === 1){
+        this.defaultInner = m.pop();
+    }
+
+    while (m.length > 0){
+    
+        this.conditionals.push(m.shift());
+        this.inners.push(m.shift());
+
+    }
+    
+    this.codes = [];
+    for(var i = 0; i < this.inners.length; i++){
+        this.codes[i] = decode(this.inners[i]);
+    }
+
+    if(typeof(this.defaultInner) !== "undefined"){
+        this.defaultCode = decode(this.defaultInner);
+    }
 
 }
 
